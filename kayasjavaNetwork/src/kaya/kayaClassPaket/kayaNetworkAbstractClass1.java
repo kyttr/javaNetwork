c41be3c63ed95fe1d2148d1f667b363d34e8b425
kayatatar <kayatatar@yahoo.com>
2012-07-15 13:51:40 +0300

    "httpURLConnectionBilgileri()" metodu tamamlandı.

--- kayasjavaNetwork/src/kaya/kayaClassPaket/kayaNetworkAbstractClass1.java
+++ kayasjavaNetwork/src/kaya/kayaClassPaket/kayaNetworkAbstractClass1.java
@@ -218,7 +218,7 @@ public abstract class kayaNetworkAbstractClass1 {
 
         return bilgilerDon(props, description, listeVals);
     }
-    
+
     /*
      * returns : information corresponding to a "URLConnection" object return
      * LinkedList<LinkedList<String>> each LinkedList<String> has size 3.
@@ -228,34 +228,33 @@ public abstract class kayaNetworkAbstractClass1 {
      * 2. element = name of the property
      *
      * 3. element = description of the property
-     */    
-    public static LinkedList<LinkedList<String>> urlConnectionBilgileri(URLConnection urlBaglanti) throws IOException
-    {
+     */
+    public static LinkedList<LinkedList<String>> urlConnectionBilgileri(URLConnection urlBaglanti) throws IOException {
         LinkedList<String> listeVals = new LinkedList<String>();
 
         listeVals.add(String.valueOf(urlBaglanti.getAllowUserInteraction()));
-listeVals.add(String.valueOf(urlBaglanti.getConnectTimeout()));
-listeVals.add(String.valueOf(urlBaglanti.getContent()));
-listeVals.add(String.valueOf(urlBaglanti.getContentEncoding()));
-listeVals.add(String.valueOf(urlBaglanti.getContentLength()));
-listeVals.add(String.valueOf(urlBaglanti.getContentLengthLong()));
-listeVals.add(String.valueOf(urlBaglanti.getContentType()));
-listeVals.add(String.valueOf(urlBaglanti.getDate()));
-listeVals.add(String.valueOf(urlBaglanti.getDefaultAllowUserInteraction()));
-listeVals.add(String.valueOf(urlBaglanti.getDefaultUseCaches()));
-listeVals.add(String.valueOf(urlBaglanti.getDoInput()));
-listeVals.add(String.valueOf(urlBaglanti.getDoOutput()));
-listeVals.add(String.valueOf(urlBaglanti.getExpiration()));
-listeVals.add(String.valueOf(urlBaglanti.getFileNameMap()));
-listeVals.add(String.valueOf(urlBaglanti.getHeaderFields()));
-listeVals.add(String.valueOf(urlBaglanti.getIfModifiedSince()));
-listeVals.add(String.valueOf(urlBaglanti.getLastModified()));
-listeVals.add(String.valueOf(urlBaglanti.getPermission()));
-listeVals.add(String.valueOf(urlBaglanti.getReadTimeout()));
-listeVals.add(String.valueOf(urlBaglanti.getRequestProperties()));
-listeVals.add(String.valueOf(urlBaglanti.getURL()));
-listeVals.add(String.valueOf(urlBaglanti.getUseCaches()));
-listeVals.add(String.valueOf(urlBaglanti.toString()));
+        listeVals.add(String.valueOf(urlBaglanti.getConnectTimeout()));
+        listeVals.add(String.valueOf(urlBaglanti.getContent()));
+        listeVals.add(String.valueOf(urlBaglanti.getContentEncoding()));
+        listeVals.add(String.valueOf(urlBaglanti.getContentLength()));
+        listeVals.add(String.valueOf(urlBaglanti.getContentLengthLong()));
+        listeVals.add(String.valueOf(urlBaglanti.getContentType()));
+        listeVals.add(String.valueOf(urlBaglanti.getDate()));
+        listeVals.add(String.valueOf(urlBaglanti.getDefaultAllowUserInteraction()));
+        listeVals.add(String.valueOf(urlBaglanti.getDefaultUseCaches()));
+        listeVals.add(String.valueOf(urlBaglanti.getDoInput()));
+        listeVals.add(String.valueOf(urlBaglanti.getDoOutput()));
+        listeVals.add(String.valueOf(urlBaglanti.getExpiration()));
+        listeVals.add(String.valueOf(urlBaglanti.getFileNameMap()));
+        listeVals.add(String.valueOf(urlBaglanti.getHeaderFields()));
+        listeVals.add(String.valueOf(urlBaglanti.getIfModifiedSince()));
+        listeVals.add(String.valueOf(urlBaglanti.getLastModified()));
+        listeVals.add(String.valueOf(urlBaglanti.getPermission()));
+        listeVals.add(String.valueOf(urlBaglanti.getReadTimeout()));
+        listeVals.add(String.valueOf(urlBaglanti.getRequestProperties()));
+        listeVals.add(String.valueOf(urlBaglanti.getURL()));
+        listeVals.add(String.valueOf(urlBaglanti.getUseCaches()));
+        listeVals.add(String.valueOf(urlBaglanti.toString()));
 
         String props = "getAllowUserInteraction,getConnectTimeout,getContent,getContentEncoding,getContentLength,getContentLengthLong,getContentType,getDate,getDefaultAllowUserInteraction,getDefaultUseCaches,getDoInput,getDoOutput,getExpiration,getFileNameMap,getHeaderFields,getIfModifiedSince,getLastModified,getPermission,getReadTimeout,getRequestProperties,getURL,getUseCaches,toString";
 
@@ -264,7 +263,44 @@ listeVals.add(String.valueOf(urlBaglanti.toString()));
             description = "Returns the value of the allowUserInteraction field for this object.:::Returns setting for connect timeout.:::Retrieves the contents of this URL connection.:::Returns the value of the content-encoding header field.:::Returns the value of the content-length header field.:::Returns the value of the content-length header field as a long.:::Returns the value of the content-type header field.:::Returns the value of the date header field.:::Returns the default value of the allowUserInteraction field.:::Returns the default value of a URLConnection's useCaches flag.:::Returns the value of this URLConnection's doInput flag.:::Returns the value of this URLConnection's doOutput flag.:::Returns the value of the expires header field.:::Loads filename map (a mimetable) from a data file.:::Returns an unmodifiable Map of the header fields.:::Returns the value of this object's ifModifiedSince field.:::Returns the value of the last-modified header field.:::Returns a permission object representing the permission necessary to make the connection represented by this object.:::Returns setting for read timeout.:::Returns an unmodifiable Map of general request properties for this connection.:::Returns the value of this URLConnection's URL field.:::Returns the value of this URLConnection's useCaches field.:::Returns a String representation of this URL connection.";
         }
 
-        return bilgilerDon(props, description, listeVals);        
+        return bilgilerDon(props, description, listeVals);
+    }
+
+    /*
+     * returns : information corresponding to a "HttpURLConnection" object
+     * return LinkedList<LinkedList<String>> each LinkedList<String> has size 3.
+     *
+     * 1. element = method that returns a property
+     *
+     * 2. element = name of the property
+     *
+     * 3. element = description of the property
+     */
+    public static LinkedList<LinkedList<String>> httpURLConnectionBilgileri(HttpURLConnection httpURLBaglanti) throws IOException {
+        LinkedList<String> listeVals = new LinkedList<String>();
+
+        listeVals.add(String.valueOf(httpURLBaglanti.getErrorStream()));
+        listeVals.add(String.valueOf(httpURLBaglanti.getFollowRedirects()));
+        listeVals.add(String.valueOf(httpURLBaglanti.getInstanceFollowRedirects()));
+        listeVals.add(String.valueOf(httpURLBaglanti.getPermission()));
+        listeVals.add(String.valueOf(httpURLBaglanti.getRequestMethod()));
+        listeVals.add(String.valueOf(httpURLBaglanti.getResponseCode()));
+        listeVals.add(String.valueOf(httpURLBaglanti.getResponseMessage()));
+        listeVals.add(String.valueOf(httpURLBaglanti.usingProxy()));
+
+        String props = "getErrorStream,getFollowRedirects,getInstanceFollowRedirects,getPermission,getRequestMethod,getResponseCode,getResponseMessage,usingProxy";
+
+        String description;
+        {
+            description = "Returns the error stream if the connection failed but the server sent useful data nonetheless.:::Returns a boolean indicating whether or not HTTP redirects (3xx) should be automatically followed.:::Returns the value of this HttpURLConnection's instanceFollowRedirects field.:::Returns a SocketPermission object representing the permission necessary to connect to the destination host and port.:::Get the request method.:::Gets the status code from an HTTP response message.An int representing the three digit HTTP Status-Code.1xx: Informational2xx: Success3xx: Redirection4xx: Client Error5xx: Server ErrorGets the status code from an HTTP response message. For example, in the case of the following status lines:HTTP/1.0 200 OKHTTP/1.0 401 Unauthorized:::Gets the HTTP response message, if any, returned along with the response code from a server.Gets the HTTP response message, if any, returned along with the response code from a server. From responses like:HTTP/1.0 200 OKHTTP/1.0 404 Not FoundExtracts the Strings \"OK\" and \"Not Found\" respectively. Returns null if none could be discerned from the responses (the result was not valid HTTP).:::Indicates if the connection is going through a proxy.";
+        }
+
+        // Bir "HttpURLConnection" nesnesi aynı zamanda bir "URLConnection" nesnesidir. Bu nedenle "urlConnectionBilgileri()" metodundan gelen bilgiler de bir "HttpURLConnection" nesnesinin bilgilerinde gözükmeli. Bunun için superclass olan "URLConnection"daki bilgileri de aldım.
+        LinkedList<LinkedList<String>> httpUrlConnectionInfos = bilgilerDon(props, description, listeVals);
+        LinkedList<LinkedList<String>> UrlConnectionInfos = urlConnectionBilgileri(httpURLBaglanti);
+
+        httpUrlConnectionInfos.addAll(UrlConnectionInfos);
+        return httpUrlConnectionInfos;
     }
 
     /*
@@ -357,7 +393,7 @@ listeVals.add(String.valueOf(urlBaglanti.toString()));
      */
     public static String readURLDirectly2HTMLString(URL url) throws IOException {
         // "InputStream" den "String" okumak için yeni bir metot yazdım, artık onu kullanıyorum.
-        InputStream is=url.openStream();
+        InputStream is = url.openStream();
         return GenelMetotlar.readStringfromInputStream(is);
     }
 
@@ -1064,7 +1100,7 @@ listeVals.add(String.valueOf(urlBaglanti.toString()));
 
         return epostaProps;
     }
-    
+
     /*
      * returns : information corresponding to a "NetworkInterface" object return
      * LinkedList<LinkedList<String>> each LinkedList<String> has size 3.
@@ -1075,8 +1111,7 @@ listeVals.add(String.valueOf(urlBaglanti.toString()));
      *
      * 3. element = description of the property
      */
-    public LinkedList<LinkedList<String>> networkInterfaceBilgileri(NetworkInterface ni) throws SocketException 
-    {
+    public LinkedList<LinkedList<String>> networkInterfaceBilgileri(NetworkInterface ni) throws SocketException {
         LinkedList<String> listeVals = new LinkedList<String>();
 
         listeVals.add(String.valueOf(ni.getDisplayName()));
